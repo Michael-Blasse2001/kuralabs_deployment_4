e51fde85f880aeaa81138e729e66b525963a37e9
Michael-Blasse2001 <98329852+Michael-Blasse2001@users.noreply.github.com>
2022-10-27 23:13:00 -0400

    Update main.tf

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -11,6 +11,7 @@ provider "aws" {
 resource "aws_instance" "web_server01" {
   ami = "ami-08c40ec9ead489470"
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.pubsubnet1.id
   key_name = "JenKey"
   vpc_security_group_ids = [aws_security_group.web_ssh.id]
 
