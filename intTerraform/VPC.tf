1d2b4ae73edd76eb34ef922cebbd7c3c96ec59ed
Michael-Blasse2001 <98329852+Michael-Blasse2001@users.noreply.github.com>
2022-10-27 23:32:10 -0400

    Update VPC.tf

--- intTerraform/VPC.tf
+++ intTerraform/VPC.tf
@@ -8,14 +8,14 @@ resource "aws_vpc" "KuraVpc" {
 #SUBNETS
 
 resource "aws_subnet" "pubsubnet1" {
-  idr_block              = "172.19.0.0/18"
+  cidr_block              = "172.19.0.0/18"
   vpc_id                  = aws_vpc.KuraVpc.id
   map_public_ip_on_launch = "true"
   availability_zone       = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_subnet" "pubsubnet2" {
-  idr_block              = "172.19.0.0/18"
+  cidr_block              = "172.19.0.0/18"
   vpc_id                  = aws_vpc.KuraVpc.id
   map_public_ip_on_launch = "true"
   availability_zone       = data.aws_availability_zones.available.names[0]
